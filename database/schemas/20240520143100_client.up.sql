c3bffbb887e77dfa2922bb4ace5ef9ac86eb62f8
Joseph <josephsilva0121@gmail.com>
2024-05-24 18:48:44 -0500

    refactor(migrations): improve migration scripts structure client

---
+++ database/schemas/20240520143100_client.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE client (
+  client_id BIGSERIAL PRIMARY KEY,
+  given_name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP
+);
