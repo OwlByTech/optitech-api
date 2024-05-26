09e859a94a72a2c4160701c3e9f6252140ae26aa
Joseph <josephsilva0121@gmail.com>
2024-05-26 00:34:24 -0500

    feat: Strategy Pattern for seeders (need review)

--- database/seeder.go
+++ database/seeder.go
@@ -8,33 +8,13 @@ import (
 	sq "optitech/internal/sqlc"
 )
 
-func Seeder(arg string) error {
-	db, err := Connect()
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	repository.Queries = *sq.New(db)
-
-	switch arg {
-	case "up":
-		if err := seederUp(); err != nil {
-			return err
-		}
-
-	case "down":
-		if err := seederDown(); err != nil {
-			return err
-		}
-
-	default:
-		return fmt.Errorf("You must provide up or down arguments")
-	}
-
-	return nil
+type SeederStrategy interface {
+	Execute() error
 }
 
-func seederUp() error {
+type SeederUp struct{}
+
+func (s SeederUp) Execute() error {
 	if err := seeders.ClientUp(); err != nil {
 		return err
 	}
@@ -47,10 +27,21 @@ func seederUp() error {
 	if err := seeders.FormatUp(); err != nil {
 		return err
 	}
+	if err := seeders.DocumentsUp(); err != nil {
+		return err
+	}
+	if err := seeders.DocumentClientUp(); err != nil {
+		return err
+	}
+	if err := seeders.InstitutionClientUp(); err != nil {
+		return err
+	}
 	return nil
 }
 
-func seederDown() error {
+type SeederDown struct{}
+
+func (s SeederDown) Execute() error {
 	if err := seeders.FormatDown(); err != nil {
 		return err
 	}
@@ -63,6 +54,35 @@ func seederDown() error {
 	if err := seeders.InstitutionDown(); err != nil {
 		return err
 	}
-
+	if err := seeders.DocumentsDown(); err != nil {
+		return err
+	}
+	if err := seeders.DocumentClientDown(); err != nil {
+		return err
+	}
+	if err := seeders.InstitutionClientDown(); err != nil {
+		return err
+	}
 	return nil
+}
+func Seeder(arg string) error {
+	db, err := Connect()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	repository.Queries = *sq.New(db)
+
+	var strategy SeederStrategy
+
+	switch arg {
+	case "up":
+		strategy = SeederUp{}
+	case "down":
+		strategy = SeederDown{}
+	default:
+		return fmt.Errorf("You must provide up or down arguments")
+	}
+
+	return strategy.Execute()
 }
