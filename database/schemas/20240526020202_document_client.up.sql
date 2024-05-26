e0ff442e339b3e5a16237578916a56d8e9e818be
Joseph <josephsilva0121@gmail.com>
2024-05-25 23:37:21 -0500

    fix: update not null values

--- database/schemas/20240526020202_document_client.up.sql
+++ database/schemas/20240526020202_document_client.up.sql
@@ -2,8 +2,8 @@ CREATE TYPE action AS ENUM ('borrado', 'actualizado', 'creado');
 
 CREATE TABLE document_client(
     document_client_id BIGSERIAL PRIMARY KEY,
-    client_id INT REFERENCES client(client_id),
-    document_id INT REFERENCES document(document_id),
+    client_id INT REFERENCES client(client_id) NOT NULL,
+    document_id INT REFERENCES document(document_id) NOT NULL,
     action action NOT NULL,
     create_at TIMESTAMP NOT NULL
 );
