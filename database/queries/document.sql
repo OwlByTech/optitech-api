3fc94c918d1c5bb0cbbafed911fdb227c305528f
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-06-03 23:28:33 -0500

    feat: remove client_id field

--- database/queries/document.sql
+++ database/queries/document.sql
@@ -7,26 +7,26 @@ SELECT * FROM document
 ORDER BY document_id;
 
 -- name: GetDocumentByName :one
-SELECT format_id, institution_id, client_id, url, status
+SELECT directory_id, format_id, url, status
 FROM document
 WHERE document_id = $1;
 
 -- name: CreateDocument :one
-INSERT INTO document(format_id, institution_id, client_id, url, status, created_at)
-VALUES ($1, $2, $3, $4, $5, $6)
+INSERT INTO document(directory_id, format_id, url, status, created_at)
+VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
 -- name: UpdateDocumentById :exec
 UPDATE document
-SET  = $2, status = $3, updated_at = $4
+SET  directory_id = $2 format_id = $3 url = $4, status = $5, updated_at = $6
 WHERE document_id = $1;
 
 -- name: DeleteDocumentById :exec
 UPDATE document
 SET deleted_at = $2
-WHERE document = $1
+WHERE document_id = $1
 
 -- name: DeleteAllDocuments :execresult
 UPDATE document;
 SET deleted_at = $1
-WHERE deleted_at IS NULL;
+WHERE document_id IS NULL;
