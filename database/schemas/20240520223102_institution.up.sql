7125443e7b06e8de461b278bf07991c79b28b33c
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-06-04 00:09:15 -0500

    feat: update institutions fields

--- database/schemas/20240520223102_institution.up.sql
+++ database/schemas/20240520223102_institution.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE institution (
   institution_id BIGSERIAL PRIMARY KEY,
   asesor_id INT REFERENCES asesor(asesor_id),
+  services_id INT REFERENCES services(services_id) NOT NULL,
   institution_name VARCHAR(50) NOT NULL,
   logo VARCHAR(255),
   description  VARCHAR(255) NOT NULL,
-  services text[] NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP,  
   deleted_at TIMESTAMP,
