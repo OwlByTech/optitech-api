41574d952313fddcb26dc91e569ca033f04a7ae1
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-06-04 19:12:19 -0500

    :feat: add not null values

--- database/schemas/20240604002700_roles.up.sql
+++ database/schemas/20240604002700_roles.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE roles(
     role_id BIGSERIAL PRIMARY KEY, 
-    role_name VARCHAR(50),
+    role_name VARCHAR(50) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP
