a1b03983917694619e59a75421734b6f671a4481
CamiloArias12 <ariaspira13@gmail.com>
2024-06-06 18:37:48 -0500

    feat:add handler institution

---
+++ internal/handler/institution.go
@@ -0,0 +1,71 @@
+package handler
+
+import (
+	"encoding/json"
+	dto "optitech/internal/dto"
+	cdto "optitech/internal/dto/institution"
+	"optitech/internal/service/institution"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetInstitutionHandler(c *fiber.Ctx) error {
+	params := c.AllParams()
+	req := &cdto.GetInstitutionReq{}
+	if err := dto.ValidateParamsToDTO(params, req); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	res, err := service.GetInstitutionService(*req)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(res)
+}
+
+func ListInstitutionsHandler(c *fiber.Ctx) error {
+
+	res, err := service.ListInstitutionsService()
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(res)
+}
+
+func CreateInstitutionHandler(c *fiber.Ctx) error {
+
+	req := &cdto.CreateInstitutionReq{}
+	params := c.FormValue("data")
+	if err := json.Unmarshal([]byte(params), &req); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+	file, err := c.FormFile("file")
+	req.LogoFile = file
+	res, err := service.CreateInstitutionService(*req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(res)
+}
+
+func DeleteInstitutionHandler(c *fiber.Ctx) error {
+
+	params := c.AllParams()
+	req := &cdto.GetInstitutionReq{}
+	if err := dto.ValidateParamsToDTO(params, req); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	res, err := service.DeleteInstitutionService(*req)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(res)
+
+}
