017103f62b23b4510db961ba904046433a3a4038
CamiloArias12 <ariaspira13@gmail.com>
2024-06-06 18:36:28 -0500

    feat:add routes institution

---
+++ internal/router/institution.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"optitech/internal/handler"
+)
+
+func (s Server) AttachRoutes() {
+	r := s.app
+	institution_route := r.Group("/api/institution")
+	institution_route.Post("/", handler.CreateInstitutionHandler)
+	institution_route.Get("/:id", handler.GetInstitutionHandler)
+	institution_route.Get("/", handler.ListInstitutionsHandler)
+	institution_route.Delete("/:id", handler.DeleteInstitutionHandler)
+}
