cc9423dc80ee3224e15d7467edf78df848ba3658
CamiloArias12 <ariaspira13@gmail.com>
2024-06-07 21:09:03 -0500

    refactor:update primary key

--- database/queries/institution_services.sql
+++ database/queries/institution_services.sql
@@ -1,25 +1,12 @@
--- name: GetInstitutionService :one
-SELECT * FROM institution_services
-WHERE institution_services_id = $1 LIMIT 1;
-
 -- name: ListInstitutionServices :many
-SELECT * FROM institution_services
-ORDER BY institution_services_id;
+SELECT services.service_name ,services.service_id FROM institution_services
+INNER JOIN services ON  institution_services.services_id=services.service_id
+WHERE institution_services.institution_id= $1
+ORDER BY services.service_id;
 
--- name: GetInstitutionServiceByName :one
-SELECT institution_id, services_id
-FROM institution_services
-WHERE institution_services_id = $1;
-
--- name: CreateInstitutionService :one
-INSERT INTO institution_services(institution_id, services_id, created_at)
-VALUES ($1, $2, $3)
-RETURNING *;
-
--- name: DeleteInstitutionServiceById :exec
-UPDATE institution_services
-SET deleted_at = $2
-WHERE institution_services_id = $1;
+-- name: CreateInstitutionService :copyfrom
+INSERT INTO institution_services(institution_id, service_id, created_at)
+VALUES ($1, $2, $3);
 
 -- name: DeleteAllInstitutionServices :execresult
 UPDATE institution_services
