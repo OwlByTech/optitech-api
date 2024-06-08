b9aecb375d6a8be0d59f02d3411b65d0924f7a1c
CamiloArias12 <ariaspira13@gmail.com>
2024-06-07 21:09:44 -0500

    feat:add querys services

--- database/queries/services.sql
+++ database/queries/services.sql
@@ -1,15 +1,15 @@
 -- name: GetServices :one
 SELECT * FROM services
-WHERE services_id = $1 LIMIT 1;
+WHERE service_id = $1 LIMIT 1;
 
 -- name: ListServicess :many
 SELECT * FROM services
-ORDER BY services_id;
+ORDER BY service_id;
 
 -- name: GetServicesByName :one
 SELECT service_name
 FROM services
-WHERE services_id = $1;
+WHERE service_id = $1;
 
 -- name: CreateServices :one
 INSERT INTO services(service_name, created_at)
@@ -19,12 +19,12 @@ RETURNING *;
 -- name: UpdateServicesById :exec
 UPDATE services
 SET service_name = $2, updated_at = $3
-WHERE services_id = $1;
+WHERE service_id = $1;
 
 -- name: DeleteServicesById :exec
 UPDATE services
 SET deleted_at = $2
-WHERE services_id = $1;
+WHERE service_id = $1;
 
 -- name: DeleteAllServicess :execresult
 UPDATE services
