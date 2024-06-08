a937bf463491719923561a206552adebbc7d1ecd
CamiloArias12 <ariaspira13@gmail.com>
2024-06-07 21:12:28 -0500

    feat:change paramns institution_services

--- internal/dto/institution_services/create.go
+++ internal/dto/institution_services/create.go
@@ -2,11 +2,11 @@ package dto
 
 type CreateInstitutionServicesReq struct {
 	InstitutionID int64 `json:"institution_id" validate:"required"`
-	ServicesId    int64 `json:"services_id" validate:"required"`
+	ServiceId     int64 `json:"services_id" validate:"required"`
 }
 
 type CreateInstitutionServicesRes struct {
 	Id            int64
 	InstitutionID int64
-	ServicesId    int64
+	ServiceId     int64
 }
