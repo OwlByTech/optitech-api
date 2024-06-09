7733ac5159520967dfd6d23063c7f6a28534a5ca
CamiloArias12 <ariaspira13@gmail.com>
2024-06-09 11:52:28 -0500

    feat:add institution routes

--- internal/router/institution.go
+++ internal/router/institution.go
@@ -2,16 +2,18 @@ package router
 
 import (
 	"optitech/internal/handler"
+	"optitech/internal/repository"
+	service "optitech/internal/service/institution"
 )
 
-func (s Server) RoutesInstitution() {
+func (s *Server) RoutesInstitution() {
 	r := s.app
+	repo := repository.NewRepositoryInstitution(&repository.Queries)
+	sevice := service.NewServiceInstitution(repo)
+	handler := handler.NewServiceInstitution(sevice)
 	institution_route := r.Group("/api/institution")
-	institution_route.Post("/", handler.CreateInstitutionHandler)
-	institution_route.Put("/:id", handler.UpdateInstitutionHandler)
-	institution_route.Delete("/:id", handler.DeleteInstitutionHandler)
-	institution_route.Get("/:id", handler.GetInstitutionHandler)
-	institution_route.Get("/", handler.ListInstitutionsHandler)
-	institution_route.Delete("/:id", handler.DeleteInstitutionHandler)
+	institution_route.Post("/", handler.Create)
+	institution_route.Get("/:id", handler.Get)
+	institution_route.Get("/", handler.List)
 
 }
