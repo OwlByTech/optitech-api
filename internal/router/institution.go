8f65efc8b4d6f9bccd4ea2c158b121ac598fdefe
CamiloArias12 <ariaspira13@gmail.com>
2024-06-11 08:55:47 -0500

    feat :update routes institution and services

--- internal/router/institution.go
+++ internal/router/institution.go
@@ -4,13 +4,19 @@ import (
 	"optitech/internal/handler"
 	"optitech/internal/repository"
 	service "optitech/internal/service/institution"
+	institution_client "optitech/internal/service/institution_client"
+	service_institution "optitech/internal/service/institution_services"
 )
 
 func (s *Server) RoutesInstitution() {
 	r := s.app
-	repo := repository.NewRepositoryInstitution(&repository.Queries)
-	sevice := service.NewServiceInstitution(repo)
-	handler := handler.NewServiceInstitution(sevice)
+	repo_institution_services := repository.NewRepositoryInstitutionServices(&repository.Queries)
+	service_institution_services := service_institution.NewServiceInstitutionServices(repo_institution_services)
+	repo_institution_client := repository.NewRepositoryInstitutionClient(&repository.Queries)
+	service_institution_client := institution_client.NewServiceInstitutionClient(repo_institution_client)
+	repo_institution := repository.NewRepositoryInstitution(&repository.Queries)
+	sevice_institution := service.NewServiceInstitution(repo_institution, service_institution_services, service_institution_client)
+	handler := handler.NewHandlerInstitution(sevice_institution)
 	institution_route := r.Group("/api/institution")
 	institution_route.Post("/", handler.Create)
 	institution_route.Get("/:id", handler.Get)
--- internal/router/services.go
+++ internal/router/services.go
@@ -2,13 +2,18 @@ package router
 
 import (
 	"optitech/internal/handler"
+	"optitech/internal/repository"
+	service "optitech/internal/service/services"
 )
 
 func (s *Server) RoutesServices() {
 	r := s.app
-	institution_route := r.Group("/api/services")
-	institution_route.Post("/", handler.CreateServiceHandler)
-	institution_route.Get("/:id", handler.GetServiceHandler)
-	institution_route.Get("/", handler.ListServicesHandler)
+	repo_service := repository.NewRepositoryService(&repository.Queries)
+	sevice := service.NewServiceServices(repo_service)
+	handler := handler.NewHandlerService(sevice)
+	service_route := r.Group("/api/service")
+	service_route.Post("/", handler.Create)
+	service_route.Get("/:id", handler.Get)
+	service_route.Get("/", handler.List)
 
 }
