c49357bff6badd019f59832b6ba13bba94180079
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-06-13 22:14:15 -0500

    feat: add dto client/update

---
+++ internal/dto/client/update.go
@@ -0,0 +1,15 @@
+package dto
+
+type UpdateClientReq struct {
+	ClientID  int64  `json:"client_id" validate:"required"`
+	GivenName string `json:"given_name" validate:"required"`
+	Surname   string `json:"surname" validate:"required"`
+	Password  string `json:"password" validate:"required,min=6"`
+	Email     string `json:"email" validate:"required,email"`
+}
+
+type UpdateClientRes struct {
+	GivenName string `json:"given_name" validate:"required"`
+	Surname   string `json:"surname" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+}
