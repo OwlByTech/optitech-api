2d166f8cbbc14cf099b898ede988ee897ea547fe
CamiloArias12 <ariaspira13@gmail.com>
2024-06-20 22:54:51 -0500

    feat:add dto reset-pasword

--- internal/dto/client/reset_password.go
+++ internal/dto/client/reset_password.go
@@ -3,3 +3,7 @@ package dto
 type ResetPasswordReq struct {
 	Email string `json:"email" validate:"required,email"`
 }
+
+type ResetPasswordTokenReq struct {
+	Token string `json:"email" validate:"required,email"`
+}
