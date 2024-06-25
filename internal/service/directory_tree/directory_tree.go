a20772f032c720f215a4becfdcbf68e64308853b
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-06-25 17:29:30 -0500

    refactor: change service to fix error

--- internal/service/directory_tree/directory_tree.go
+++ internal/service/directory_tree/directory_tree.go
@@ -23,7 +23,7 @@ func (s *serviceDirectoryTree) Get(req dto.GetDirectoryTreeReq) (*dto.GetDirecto
 	return s.directoryTreeRepository.GetDirectroy(req.Id)
 }
 
-func (s *serviceDirectoryTree) Create(req dto.CreateDirectoryTreeReq) (*dto.CreateDirectoryTreeRes, error) {
+func (s *serviceDirectoryTree) Create(req *dto.CreateDirectoryTreeReq) (*dto.CreateDirectoryTreeRes, error) {
 	repoReq := &sq.CreateDirectoryTreeParams{
 		ParentID:  pgtype.Int4{Int32: int32(req.ParentID), Valid: true},
 		Name:      pgtype.Text{String: req.Name, Valid: true},
