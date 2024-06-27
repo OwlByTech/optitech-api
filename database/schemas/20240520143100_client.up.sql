192605937a71ab57634793697580d3b46b544b02
CamiloArias12 <ariaspira13@gmail.com>
2024-06-27 07:01:32 -0500

    chore:update database query

--- database/schemas/20240520143100_client.up.sql
+++ database/schemas/20240520143100_client.up.sql
@@ -3,9 +3,10 @@ CREATE TABLE client (
   client_id SERIAL PRIMARY KEY,
   given_name VARCHAR(50) NOT NULL,
   surname VARCHAR(50) NOT NULL,
+  photo VARCHAR(255) ,
   email VARCHAR(50) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
-  status status_client DEFAULT 'inactivo',
+  status status_client DEFAULT 'inactivo' NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP,
   deleted_at TIMESTAMP
