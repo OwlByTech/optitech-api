05a13b87f261164aef786a7e14a3fed84cc80db2
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-07-04 16:28:58 -0500

    reafactor: create dto document

--- internal/dto/document/create.go
+++ internal/dto/document/create.go
@@ -1,13 +1,13 @@
 package dto
 
-type CreateDocumentClientReq struct {
+type CreateDocumentReq struct {
 	DirectoryId int32  `json:"directoryId" validate:"required"`
 	FormatId    int32  `json:"formatId" validate:"required"`
 	FileRute    string `json:"fileRute" validate:"required"`
 	Status      string `json:"status" validate:"required"`
 }
 
-type CreateDocumentClientRes struct {
+type CreateDocumentRes struct {
 	Id          int64  `json:"id"`
 	DirectoryId int32  `json:"directoryId"`
 	FormatId    int32  `json:"formatId"`
