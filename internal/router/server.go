5b9959f874f2325526f9b8e744d4a0eb16c2ce0f
CamiloArias12 <ariaspira13@gmail.com>
2024-07-08 13:00:06 -0500

    feat:add file upload get

--- internal/router/server.go
+++ internal/router/server.go
@@ -29,6 +29,7 @@ func (s *Server) ListenAndServe() error {
 	s.RoutesServices()
 	s.RoutesInstitution()
 	s.RoutesInstitutionClient()
+	s.RoutesFile()
 	err := s.app.Listen(fmt.Sprintf(":%d", s.Port))
 
 	if err != nil {
