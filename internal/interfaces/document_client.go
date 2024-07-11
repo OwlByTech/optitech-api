ac7c2fd4692507ef5f7698d5f8a3c2c9258f03e9
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-07-11 17:21:28 -0500

    feat: add interfaces

--- internal/interfaces/document_client.go
+++ internal/interfaces/document_client.go
@@ -2,18 +2,22 @@ package interfaces
 
 import (
 	dc "optitech/internal/dto/document_client"
+	models "optitech/internal/sqlc"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type IDocumentClientService interface {
-	Get(req dc.GetDocumentClientReq) (*dc.GetDocumentClientRes, error)
+	GetDocumentClient(req dc.GetDocumentClientReq) (*dc.GetDocumentClientRes, error)
+	CreateDocumentClient(arg *dc.CreateDocumentClientReq) (*dc.CreateDocumentClientRes, error)
 }
 
 type IDocumentClientRepository interface {
 	GetDocumentClient(DocumentClientID int32) (*dc.GetDocumentClientRes, error)
+	CreateDocumentClient(arg *models.CreateDocumentClientParams) (*dc.CreateDocumentClientRes, error)
 }
 
 type IDocumentClientHandler interface {
-	Get(dc *fiber.Ctx) error
+	GetDocumentClient(f *fiber.Ctx) error
+	CreateDocumentClient(f *fiber.Ctx) error
 }
