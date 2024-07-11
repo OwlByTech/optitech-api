4ca35fc1c955a4f8492aa9222df466066c0a967c
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-07-11 17:21:11 -0500

    feat: add create handler

--- internal/handler/document_client.go
+++ internal/handler/document_client.go
@@ -18,14 +18,33 @@ func NewhandlerDocumentClient(f interfaces.IDocumentClientService) interfaces.ID
 	}
 }
 
-func (h *handlerDocumentClient) Get(c *fiber.Ctx) error {
+func (h *handlerDocumentClient) GetDocumentClient(c *fiber.Ctx) error {
 	params := c.AllParams()
 	req := &ddto.GetDocumentClientReq{}
 	if err := dto.ValidateParamsToDTO(params, req); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
-	res, err := h.documentClientService.Get(*req)
+	res, err := h.documentClientService.GetDocumentClient(*req)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(res)
+}
+
+func (h *handlerDocumentClient) CreateDocumentClient(c *fiber.Ctx) error {
+	req := &ddto.CreateDocumentClientReq{}
+
+	if err := c.BodyParser(req); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid input: "+err.Error())
+	}
+
+	if err := dto.ValidateDTO(req); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	res, err := h.documentClientService.CreateDocumentClient(req)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
