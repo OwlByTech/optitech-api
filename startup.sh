4b3e073aba801f62ce6634aeeed3e8ccbbb1d14f
camilo-camargo <camilocamargo49@gmail.com>
2024-07-12 12:47:35 -0500

    feat: add building of cli with dependencies

--- startup.sh
+++ startup.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
+// TODO: Add checks for every command
+if [ $SETUP = 'first-time' ]; then
+  echo "Installing mjml"
+  apk add npm
+  npm install -g mjml
+
+  echo "Bulding cli..."
+  go build -o bin/cli cmd/cli/main.go
+
+  echo "Running Migration..."
+  ./bin/cli migrate up
+
+  echo "Running Seeders..."
+  ./bin/cli seed up
+
+  echo "Running convertions"
+  ./bin/cli convert-mjml
+fi
+
 go build -o bin/server cmd/server/main.go
 ./bin/server
