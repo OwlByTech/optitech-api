d82053297dbf892a1004cb3f2421e0bdd360c9c3
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-07-14 14:24:56 -0500

    fix: rename endopint

--- internal/router/documents.go
+++ internal/router/documents.go
@@ -15,5 +15,5 @@ func (s *Server) RoutesDocument() {
 	serviceRoute.Get("/:id", handler.Get)
 	serviceRoute.Post("/", handler.CreateDocument)
 	serviceRoute.Delete("/:id", handler.DeleteDocument)
-	serviceRoute.Get("/:id", handler.DownloadDocumentById)
+	serviceRoute.Get("download/:id", handler.DownloadDocumentById)
 }
